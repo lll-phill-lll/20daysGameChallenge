9fcdc3f98f2fe18255ef1e46bcf5b30c1f52b9f2
lll-phill-lll <mefilitov@edu.hse.ru>
2018-04-24 01:03:19 +0300

    legs printed

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,9 +1,55 @@
 #include <string>
 #include <curses.h>
+#include <iostream>
 
 // http://heather.cs.ucdavis.edu/~matloff/UnixAndC/CLanguage/Curses.pdf
 
+void draw_body(int y_center, int x_center) {
+    // int y_center = y_max - 4;
+    // int x_center = x_max / 2;
+
+    // legs
+    for (int y = y_center; y != y_center + 4; ++y) {
+        for (int x = x_center - 2; x != x_center + 3; ++x) {
+            if (x != x_center) {
+                move(y, x);
+                insch('|');
+            }
+        }
+    }
+    refresh();
+
+    // bottom
+    // y_center = y_max - 5;
+    // x_center = x_max / 2;
+    move(y_center - 1, x_center);
+    insch('_');
+    refresh();
+}
+
+
 int main() {
-    initscr();
+    WINDOW *wnd;
+    wnd = initscr();
+
+    cbreak();
+    noecho();
+
+    int nrows, ncols;
+    getmaxyx(wnd, nrows, ncols);
+
+    draw_body(nrows - 4, ncols / 2);
+    /* move(nrows - 1, ncols / 2);
+    insch('*');
+    refresh(); */
+
+    // std::cout << nrows << " " << ncols << "\n";
+
+    char d;
+    while (1) {
+        d = getch(); // curses call to input from keyboard
+        if (d == 'q') break;
+    }
+    endwin();
     return 0;
 }
--- README.md
+++ README.md
@@ -2,19 +2,17 @@
 
 ## The goal of this challenge is to make a simple terminal game without any gamedew experience. Everyday I will post sources with information I used and my steps to reach the goal.
 
---------
-##### Idea:
+### Idea:
+
 The idea is to make platform game with the symbol character, who can move around, jump and shot enemies.
 Enemies will appear from left and right sides of the game screen. Will be some save blocks with health
 regeneration and weapons upgrade.
----------
-##### Steps:
 
-* 
+### Steps:
 
 
 
-* Day1
+* [Day1](https://github.com/lll-phill-lll/20daysGameChallenge/tree/master/Day1)
 * Day2
 * Day3
 * Day4
